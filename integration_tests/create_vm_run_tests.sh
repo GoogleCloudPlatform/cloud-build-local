341d439522d8f13af441538ff4a64a0cf8cb240a
Container Builder Team <noreply@google.com>
2017-07-18 09:46:53 -0400

    Project import generated by Copybara.
    PiperOrigin-RevId: 162346017

---
+++ integration_tests/create_vm_run_tests.sh
@@ -0,0 +1,38 @@
+# Have new buckets for each test.
+DATE=`date +%Y%m%d-%H%M%S`
+GCS_PATH=gs://container-builder-local-test/$$DATE
+GCS_LOGS_PATH=gs://container-builder-local-test-logs/$$DATE
+gsutil -m copy container-builder-local $$GCS_PATH/
+gsutil -m copy ./integration_tests/* $$GCS_PATH/
+
+# Create a VM with startup script.
+gcloud config set compute/zone us-central1-f
+instance_name=ubuntu-integration-tests-$$DATE
+gcloud compute instances create $$instance_name \
+  --image-family=ubuntu-1404-lts \
+  --image-project=ubuntu-os-cloud \
+  --machine-type=n1-standard-2 \
+  --scopes default,userinfo-email,useraccounts-ro,cloud-platform \
+  --metadata-from-file startup-script=integration_tests/gce_startup_script.sh \
+  --metadata zone=us-central1-f,gcs_path=$$GCS_PATH,gcs_logs_path=$$GCS_LOGS_PATH || exit
+
+# Wait until either the success or failure file is written to GCS.
+status=0
+while true
+do
+  if gsutil stat $$GCS_LOGS_PATH/success.txt 2> /dev/null; then
+    gsutil cat $$GCS_LOGS_PATH/success.txt
+    break
+  fi
+  if gsutil stat $$GCS_LOGS_PATH/failure.txt 2> /dev/null; then
+    gsutil cat $$GCS_LOGS_PATH/failure.txt
+    status=1
+    break
+  fi
+  echo "Tests are still running, checking in a few seconds..."
+  sleep 5
+done
+
+# Delete the VM and return the status
+gcloud compute instances delete $$instance_name --quiet
+exit $$status
