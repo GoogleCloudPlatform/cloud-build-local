e5afa92d5a5346ba33fd8cf6258be5c31cd0cd8a
Edrienne <edrienne@google.com>
2018-04-24 17:30:02 -0400

    Project import generated by Copybara.
    PiperOrigin-RevId: 194146229

--- integration_tests/test_script.sh
+++ integration_tests/test_script.sh
@@ -18,6 +18,13 @@ if [ ! -f /tmp/workspace/cloudbuild_nil.yaml ]; then
   echo "Exported file not found!"
 fi
 
+# Valid substitutions
+container-builder-local --config=cloudbuild_substitutions.yaml --substitutions=_MESSAGE="bye world" . || exit
+container-builder-local --config=cloudbuild_substitutions.yaml --substitutions=COMMIT_SHA="my-sha" . || exit
+# Invalid substitutios are expected to exit with an error (hence the `&& exit`).
+container-builder-local --config=cloudbuild_substitutions.yaml --substitutions=PROJECT_ID="my-project" . && exit
+container-builder-local --config=cloudbuild_builtin_substitutions.yaml . && exit
+
 # End to end tests.
 container-builder-local --config=cloudbuild_nil.yaml --dryrun=false . || exit
 container-builder-local --config=cloudbuild_nil.yaml --dryrun=false --no-source=true || exit
@@ -26,6 +33,7 @@ container-builder-local --config=cloudbuild_dockerfile.yaml --dryrun=false . || 
 container-builder-local --config=cloudbuild_gcr.yaml --dryrun=false --push=true . || exit
 container-builder-local --config=cloudbuild_big.yaml --dryrun=false --push=true . || exit
 container-builder-local --config=cloudbuild_volumes.yaml --dryrun=false . || exit
+container-builder-local --config=cloudbuild_buildid.yaml --dryrun=false . || exit
 
 # Confirm that we set up credentials account correctly.
 WANT=$(gcloud config list --format="value(core.account)")
